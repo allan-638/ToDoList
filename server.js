4f2250cdad9106ed1ef6c7d65f8077f601b1c0e2
Allan <allanluk.ca@gmail.com>
2016-08-21 17:25:20 -0400

    - Adding "strict mode" - Fixing status codes + message logging - For updating items, only requires task OR completion state not BOTH

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+// Strict mode
+'use strict';
+
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
@@ -39,10 +42,12 @@ app.get('/api', function (req, res) {
 app.get('/api/todolist', function (req, res) {
 	return listItemModel.find(function (err, toDoList) {
 		if(err) {
-			return console.log(err);
+			console.log(err);
+			return res.status(404).send("Items cannot be found.");
 		}
 
-		return res.send(toDoList);
+		console.log("Successfully obtained full list of to-do items.");
+		return res.status(200).send(toDoList);
 	});
 });
 
@@ -50,95 +55,130 @@ app.get('/api/todolist', function (req, res) {
 app.get('/api/todolist/complete', function (req, res) { 
 	return listItemModel.find(function (err, toDoList) { 
 		if(err) { 
-			return console.log(err);
+			console.log(err);
+			return res.status(404).send("Items cannot be found.");
 		}
 
 		toDoList = toDoList.filter(function(processedList){
 			return processedList.complete == true;
 		});
 
-		return res.send(toDoList);
+		console.log("Successfully obtained list of completed to-do items.");
+		return res.status(200).send(toDoList);
 	});
 });
 
-// Obtain list of not completed items
+// Obtain list of incomplete items
 app.get('/api/todolist/incomplete', function (req, res) { 
 	return listItemModel.find(function (err, toDoList) { 
 		if(err) { 
-			return console.log(err);
+			console.log(err);
+			return res.status(404).send("Items cannot be found.");
 		}
 
 		toDoList = toDoList.filter(function(processedList) {
 			return processedList.complete == false;
 		});
 
-		return res.send(toDoList);
+		console.log("Successfully obtained list of incomplete to-do items.");
+		return res.status(200).send(toDoList);
 	});
 });
 
 // Add To-Do Item
 app.post('/api/todolist', function (req, res) {
-	var listItem;
-	listItem = new listItemModel({
+	if(!req || !req.body.task) {
+		console.log("Missing input - error in creating to-do item!"); 
+		return res.status(400).send("Missing input - error in creating to-do item!");
+	}
+
+	var listItem = new listItemModel({
 		task: req.body.task,
 		complete: req.body.complete
 	});
 
-	listItem.save(function (err) {
+	listItem.save(function(err) {
 		if(err) { 
-			return console.log(err);	
+			console.log(err);	
+			return res.status(500).send("Unknown error occurred.");
 		} 
 
-		return console.log("Created Item.");
+		console.log("Successfully created Item.");
+		return res.status(201).send(listItem);
 	});
-	return res.send(listItem);
 });
 
 // Update To-Do Item by ID
 app.put('/api/todolist/:id', function (req, res) { 
+	// Check if ID is valid
 	if((req.params.id).match(/^[0-9a-fA-F]{24}$/)) {
+		// Finding the item using the ID
 		listItemModel.findById({_id: req.params.id }, function(err, listItem) {
+			// If there's a server error
 			if(err) {
 				console.log(err);
-			} else if(listItem) {
-				listItem.task = req.body.task;
-				listItem.complete = req.body.complete;
-
-				listItem.save(function (err, listItem) {
-					if(err || !listItem) {
-						console.log("Failed to update item.");
-						return res.status(404).send("404 - Item Not Found.");
-					} else if(listItem) {
-						console.log("Updated item!");
-						return res.send(listItem);
-					}
-				});
-			} else {
-				console.log("Failed to update item.");
-				return res.status(404).send("404 - Item Not Found.");
+				return res.status(500).send("Unknown server error.");
+			// If an item can't be found with the ID
+			} else if(!listItem) {
+				console.log("Item cannot be found.");
+				return res.status(404).send("Item cannot be found.");
 			}
+
+			if(!req.body.task && !req.body.complete) { 
+				console.log("No input entered for updating item.");
+				return res.status(400).send("No input entered for updating item.");
+			}
+
+			// If "task" entered in request
+			if(req.body.task) {
+				listItem.task = req.body.task;
+			}
+
+			// If "complete" entered in request
+			if(req.body.complete) {
+				listItem.complete = req.body.complete;
+			}
+
+			// Save change to listItem
+			listItem.save(function (err, listItem) {
+				if(err) {
+					console.log("Unknown server error.");
+					return res.status(500).send("Unknown server error.");
+				} else if (!listItem) {
+					console.log("Failed to update item.");
+					return res.status(404).send("Item Not Found.");
+				} 
+				
+				console.log("Updated item!");
+				return res.status(200).send(listItem);		
+			});
 		});
 	} else {
-		console.log("Failed to update item.");
-		return res.status(404).send("404 - Item Not Found.");
+		console.log("Invalid ID entered.");
+		return res.status(400).send("Invalid ID code entered.");
 	}
 });
 
 // Delete To-Do Item by ID
 app.delete('/api/todolist/:id', function (req, res) { 
+	// Check if ID is valid
 	if((req.params.id).match(/^[0-9a-fA-F]{24}$/)) {
+		// Finding the item using the ID
 		listItemModel.findOneAndRemove({_id: req.params.id }, function(err, listItem) {
-			if(err || !listItem) {
-				console.log("Failed to delete item.");
-				return res.status(404).send("404 - Item Not Found.");
-			} else {
-				console.log("Item Deleted.");
-			}
-			return res.send(listItem);
+			if(err) {
+				console.log("Unknown server error.");
+				return res.status(500).send("Unknown server error.");
+			} else if (!listItem) {
+				console.log("Item not found.");
+				return res.status(404).send("Item not found.");
+			} 
+
+			console.log("Item Deleted.");
+			return res.status(200).send("Deleted item " + listItem._id + ".");
 		});
 	} else { 
-		console.log("Failed to delete item.");
-		return res.status(404).send("404 - Item Not Found.");
+		console.log("Invalid ID entered.");
+		return res.status(400).send("Invalid ID code entered.");
 	}
 });
 
